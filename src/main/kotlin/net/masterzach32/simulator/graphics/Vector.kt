2cb7cfc87abd69b9dc8fa0710c4c98c091f34757
Zach Kozar <zachkozar@masterzach32.net>
2017-01-29 16:12:39 -0500

    Need to fix vector math

--- src/main/kotlin/net/masterzach32/simulator/graphics/Vector.kt
+++ src/main/kotlin/net/masterzach32/simulator/graphics/Vector.kt
@@ -2,9 +2,6 @@ package net.masterzach32.simulator.graphics
 
 import java.awt.Color
 import java.awt.Graphics2D
-import java.awt.geom.Path2D
-import java.awt.geom.AffineTransform
-import java.awt.Rectangle
 
 /**
  * @param text Text to be displayed next to the arrow
@@ -16,19 +13,18 @@ class Vector(var text: String, val x: Int, val y: Int, var magnitude: Double, va
 
     val rads = Math.toRadians(angle)
 
-    fun render(g: Graphics2D) {
+    fun render(graphics: Graphics2D) {
         if (magnitude == 0.0)
             return
-        val temp = g.color
-        g.color = color
-        val r = Rectangle(x, y, thickness, (magnitude*40).toInt())
-        val path = Path2D.Double()
-        path.append(r, false)
-        val t = AffineTransform()
-        t.rotate(Math.toRadians(angle))
-        path.transform(t)
-        g.draw(path)
-        g.fill(path)
-        g.color = temp
+        val g = graphics.create() as Graphics2D
+
+        g.dispose()
+    }
+
+    private fun drawRect(g: Graphics2D, width: Int, height: Int) {
+        if (magnitude < 0)
+            g.fillRect(x - thickness/2, y, thickness, -length)
+        else
+            g.fillRect(x - thickness/2, y - length, thickness, length)
     }
 }
